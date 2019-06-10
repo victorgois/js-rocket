7f6587f1c8259e6d1fa4b905c54f4fc0e2244fc9
victorgois <victorgois18@gmail.com>
2019-06-10 15:18:46 -0300

    finalizando

--- js/rocket.js
+++ js/rocket.js
@@ -13,14 +13,32 @@ var changeState = function(state){
         timer = setInterval(function(){
             countdownNumber = countdownNumber - 1;
             document.getElementById('countdown').innerHTML = countdownNumber;
+
+            if(countdownNumber == 3 && countdownNumber <= 6){
+                //be nervous
+                document.getElementById('nervous').className = 'nervous show';
+
+            }else{
+                document.getElementById('nervous').className = 'nervous';
+
+            }
+            if(countdownNumber == 3){
+                //cant wait...
+                document.getElementById('cant-wait').className = 'cant-wait show';
+            }else{
+                document.getElementById('cant-wait').className = 'cant-wait';
+
+            }
+
             if(countdownNumber <= 0){
                 changeState(3) 
-            }
+            };
+
         },500);
     } else if (state == 3){
         var success = setTimeout(function(){
             var randomNumber = Math.round(Math.random()*10);
-            if(randomNumber > 5){
+            if(randomNumber > 2){
                 changeState(4);
             } else {
                 changeState(5);
